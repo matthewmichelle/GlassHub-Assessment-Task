3430c9398a6e0d848546099e939f0c0fdc28b8b2
matthew.michelle <matthew.michelle00@gmail.com>
2024-02-10 16:13:19 +0200

    add function to get user details by id

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,6 +5,7 @@ import { UserRepository } from "../repositories/user.repository";
 
 @injectable()
 export class UserService {
+    
     constructor(private readonly userRepository: UserRepository) { }
 
     async saveUser(user: User): Promise<User> {
@@ -12,6 +13,20 @@ export class UserService {
     }
 
     async getAllUsers(): Promise<User[]> {
-        return await this.userRepository.find();
+        const users = await this.userRepository.find();
+        if (!users) {
+            throw new Error(`Users ${users} not found`);
+        }
+        return users;
     }
+
+     async findOneById(userId: number): Promise<User | undefined> {
+
+        const user = await this.userRepository.findOneById(userId);
+        if (!user) {
+            throw new Error(`User with id ${userId} not found`);
+        }
+        return user;
+    }
+
 }
